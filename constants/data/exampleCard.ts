fe5c2764523f75db168be83713ddece74b117e61
TracyMuso <musongoletracy@gmail.com>
2024-01-12 16:18:22 +0200

    chore(layouts-and-file-structure): update pages dir right files

---
+++ constants/data/exampleCard.ts
@@ -0,0 +1,12 @@
+export const myData = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "a@b.com",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    email: "b@c.com",
+  },
+];
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Tracy Muso",
+  description: "Professional protfolio of Tracy Muso",
+};
+
+export default function Page() {
+  return <h1>Hello, Next.js!</h1>;
+}
